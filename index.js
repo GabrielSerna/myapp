b1faf9c1a34f99d1ffe2b5a64dfb1935a255b12d
capocolo <gabrielserna.hce@gmail.com>
2020-12-15 14:26:38 +0100

    practice

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function ex01() {
     }
   };
 
-  let ageGab = utente.datiPersonali.anni;
+  let ageGab = utente.datiPersonali.anni; // 35
   
   
   let coord = [1, 2, 3];
-  let x = coord[0];
-  let Y = coord[1];
-  let Z = coord[2];
+  let x = coord[0]; // 1
+  let Y = coord[1]; // 2
+  let Z = coord[2]; // 3
 
   console.log(ageGab);
   console.log(x, y. z);
@@ -83,26 +83,28 @@ function ex04() {
 
   let [let1, let2, let3, ...names] = mixArray
 
-  console.log(let1);
-  console.log(let2);
-  console.log(let3);
-  console.log(names);
+  console.log(let1);    // 15
+  console.log(let2);    // 30
+  console.log(let3);    // 45
+  console.log(names);   // 'Gabriel', 'Federica', 'Luca'
 
 }
 
 // =============================================================================
+// SPREAD ...
 function ex05() {
 
   arrNum1 = ['1', '2', '43', '54', '75', '45'];
   arrNum2 = ['100', '200'];
-  arrLet = [...arrNum1, ...arrNum2].toString();
-  // arrLet2 = arrLet.toString();
+  arrLet1 = [...arrNum1, ...arrNum2];               // ['1', '2', '43', '54', '75', '45', '100', '200']
+  arrLet2 = [...arrNum1, ...arrNum2].toString();    // 1,2,43,54,75,45,100,200
 
-  console.log(arrLet);
+  console.log(arrLet2);
 
 }
 
 // =============================================================================
+// SPREAD ...
 function ex06() {
 
   function somma(...nums) {
@@ -122,3 +124,144 @@ function ex06() {
 };
 
 // =============================================================================
+// MAP
+function ex07() {
+  let names = [ 'Gabriel', 'Federica', 'Riccardo', 'Michele' ];
+
+  let modifyNames = names.map( x => `${x}!!`);
+  // let modifyNames = names.map( x => x + '!!' );
+
+  console.log(modifyNames); // ['Gabriel!!', 'Federica!!', 'Riccardo!!', 'Michele!!']
+
+}
+
+// =============================================================================
+// FILTER
+function ex08() {
+  let names = [ 'Gabriel', 'Federica', 'Riccardo', 'Michele', 'Luca', 'Lele', 'Francesca', 'Claudio', 'Jessica' ];
+
+  let namesTooLength = names.filter( x => x.length > 5);  // (7) ['Gabriel', 'Federica', 'Riccardo', 'Michele', 'Francesca', 'Claudio', 'Jessica']
+  let shortSnamesLength = names.filter( el => el.length < 5).map( el => el.length); // (2) [4, 4]
+
+  console.log(namesTooLength);
+  console.log(shortSnamesLength);
+
+}
+
+// =============================================================================
+// ARROW FUNCTIONS
+function ex09() {
+
+  let names = [ 'Gabriel', 'Federica', 'Riccardo', 'Michele', 'Luca', 'Lele', 'Francesca', 'Claudio', 'Jessica' ];
+
+  let namesUpper = names.map( el => el.toUpperCase());
+
+  // let namesUpper = names.map( function(el) {
+  //   return el.toUpperCase();
+  // })
+
+  console.log(namesUpper);
+}
+
+// =============================================================================
+//
+function ex10() {
+
+  const nums = [1, 2, 3, 5, 6, 10, 20, 30];
+
+  const result = nums.filter( el => el > 5).map( num => num * 2 );
+
+  console.log(result);
+
+}
+
+// =============================================================================
+// ARROW FUNCTION
+function ex11() {
+
+  let hello1 = name => `Hello ${name}`; // dichiaro il parametro <name>
+  console.log(hello1('Gabriel'));
+
+  let hello2 = () => `Ciao a tutti`;     // non passo nessun parametro
+  console.log(hello2);
+
+  let names = (name1, name2, name3) => `lo staff è composto da ${name1}, ${name2}, ${name3}`;     // passo 3 parametri
+  console.log(names('Gabriel', 'Luca', 'Riccardo'));
+
+}
+
+// =============================================================================
+// ARROW FUNCTION
+function ex12() {
+
+  let saluta = (name1, name2, name3) => {
+    name1 = name1.toUpperCase();
+    name2 = name2.toUpperCase();
+
+    return `Ciao ${name1}, il tuo nome è lungo ${name1.length} caratteri, sei insieme a ${name2} e ${name3}`;
+  }
+
+  console.log(saluta('Gabriel', 'Claudio', 'Mattia'));
+
+}
+
+// =============================================================================
+// THIS - function
+function ex13() {
+
+  function Prefisso(prefisso) {
+    this.prefisso = prefisso;
+  }
+
+  Prefisso.prototype.prefissoArray = function(arr) {
+
+    let that = this;
+    return arr.map(function(x) {
+      return that.prefisso + x;
+    });
+  };
+
+  let pre = new Prefisso('Ciao ');
+  console.log(pre.prefissoArray(['Gabriel', 'Claudio']));
+
+}
+
+// =============================================================================
+// THIS - arrow function
+// le arrow function ereditano THIS dal contesto che le contiene
+function ex14() {
+
+  function Prefisso(prefisso) {
+    this.prefisso = prefisso;
+  }
+
+  Prefisso.prototype.prefissoArray = function(arr) {
+
+    return arr.map( x => this.prefisso + x );
+    };
+
+  let pre = new Prefisso('Ciao ');
+  console.log(pre.prefissoArray(['Gabriel', 'Claudio']));
+
+}
+
+// =============================================================================
+//
+function ex15() {
+
+  function creaMatrice([ righe = 5, colonne = 3] = []) {
+
+    return `Creo una matrice ${righe} x ${colonne}`
+  }
+
+    console.log(creaMatrice([]));               // Creo una matrice 5 x 3
+    console.log(creaMatrice([2]));              // Creo una matrice 2 x 3
+    console.log(creaMatrice([2, 4]));           // Creo una matrice 2 x 4
+    console.log(creaMatrice([undefined, 6]));   // Creo una matrice 5 x 6
+    console.log(creaMatrice([, 10]));           // Creo una matrice 5 x 10
+    console.log(creaMatrice());                 // Creo una matrice 5 x 3
+
+}
+
+// =============================================================================
+//
